9afc6c3847b9195c81b163ae5b5cd5b37050b41f
SyedSayeelAbbas <syedsayeelabbas@gmail.com>
2025-04-21 10:55:52 +0500

    Add files via upload

--- task_18.cpp
+++ task_18.cpp
@@ -1,34 +1,16 @@
-/*Task # 6 Create a .cpp file with name variables.cpp and 
-write the programming statements for the following:
- 
-•	Create a variable without assigning the value,
- and assign it the value in later statement.
-
-•	Assign a new value to an existing value 
-(this will overwrite the previous value)
-
-•	Create an unchangeable variable with 
-    the const keyword
-
-•	Demonstrate the use of # define directive 
-
-•	Combine text and a variable on output  (console window)
-
-•	Add a variable to another variable
-
-•	Declare many variables of the same type 
-    with a comma-separated list Identifiers 
-
-•	Define and Initialize two string variables of your 
-    own choice and concatenate both strings and display the final value. 
-
-•	With suitable programming example explain the concept 
-    of signed and unsigned variables. (Use comments for explanation)
+/*Task # 18  A shop will give discount of 10% if the cost of purchased quantity is more than 1000.
+Ask user for quantity
+Suppose, one unit will cost 100.
+Judge and print total cost for user.
 */
 #include<iostream>
+#include<conio.h>
 using namespace std;
-int main() {
-    float num=30.33;
-    int in=num;//it  work for type casting
-    cout<<in;
+int main()
+{
+    int o,g;
+    cout<<"Enter quntity to buy :"; cin>>o;
+    g=(100*o);
+    if(g>=1000) cout<<"you get 10% discount of "<<(g*0.10)<<"Rs";
+    else cout<<"you has not purchase enough";
 }
--- task_7.cpp
+++ task_7.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    char x;
-    cout<<"Enter upper case value for lower case :";
-    cin>>x;
+    char x='Z';
+    // cout<<"Enter upper case value for lower case :";
+    // cin>>x;
     cout<<"The lower case of character "<<x<<" is :"<<(char)(x+32)<<"\nand lower case value of "<<(char)(x+32)<<" is:"<<(x+32);
     //cout<<97-x;
 }
